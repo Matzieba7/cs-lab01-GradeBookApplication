3f0626f5d2950056412f4b8d460a4ad69b7e5362
Mateusz Zięba <matzieba7@gmail.com>
2022-03-19 12:01:57 +0100

    Create RankedGradeBook Class and Update RankedGradeBook Type

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GradeBook.Enums;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks 
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
